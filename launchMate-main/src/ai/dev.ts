aeca307d72791e894fe969b8645e8e7204599811
sudhar777san-arch <sudhar777san@gmail.com>
2025-12-05 16:26:55 +0530

    Delete launchMate-main directory

--- launchMate-main/src/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-resume.ts';
-import '@/ai/flows/generate-portfolio.ts';
-import '@/ai/flows/ai-mentor-guidance.ts';
--- launchMate-main/src/ai/genkit.ts
+++
@@ -1,9 +0,0 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
-
-export const ai = genkit({
-  plugins: [googleAI({
-    apiKey: process.env.GOOGLE_GENAI_API_KEY,
-  })],
-  model: 'googleai/gemini-2.5-flash',
-});
